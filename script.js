2a06037cc87a4b60d302547933f3f99ab267c138
jiahua.li12321@gmail.com <jiahua.li12321@gmail.com>
2023-03-13 18:36:45 -0400

    made the naming consistent and removed comments

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ arrOfNodes.forEach((el) => {
 }) */
 
 // Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("star-map-modal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+let img = document.getElementById("star-map-image");
+let modalImg = document.getElementById("img01");
+let captionText = document.getElementById("caption");
 img.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
@@ -22,7 +22,7 @@ img.onclick = function(){
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
