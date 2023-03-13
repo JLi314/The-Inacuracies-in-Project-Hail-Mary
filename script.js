e7ca35b575cb210d5c408848ff2c2f1c74c3e8c8
jiahua.li12321@gmail.com <jiahua.li12321@gmail.com>
2023-03-13 16:04:26 -0400

    added the zoom in ability on the star map

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const foo = ['...', '...'];
+/*const foo = ['...', '...'];
 
 const arrOfTextNodes = document.querySelector('#id').textContent();
 
@@ -6,4 +6,26 @@ arrOfNodes.forEach((el) => {
     if (foo.includes(el)) {
         
     }
-})
+}) */
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
